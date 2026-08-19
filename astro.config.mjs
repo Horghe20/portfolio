16580a89accfa8a54ccab534a36d237925fe5ea9
giorgiodicristofalo <giorgiodicristofalo77@gmail.com>
2026-08-19 10:47:36 +0200

    chore: revert site url back to root domain now that Vercel canonical redirect is active

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.giorgiodicristofalo.com',
+  site: 'https://giorgiodicristofalo.com',
   vite: {
     plugins: [tailwindcss()]
   },
